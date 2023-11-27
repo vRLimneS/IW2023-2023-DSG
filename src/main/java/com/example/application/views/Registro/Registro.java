3c7cb9240e923ace8906ede7fd9d409b20f26545
LimneS <85122733+vRLimneS@users.noreply.github.com>
2023-11-27 20:15:26 +0100

    terminada la vista del registro

--- src/main/java/com/example/application/views/Registro/Registro.java
+++ src/main/java/com/example/application/views/Registro/Registro.java
@@ -18,10 +18,10 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
 import com.vaadin.flow.theme.lumo.LumoUtility.Padding;
 
-@PageTitle("Person Form")
-@Route(value = "person-form", layout = AppLayoutNavbar.class)
+@PageTitle("registro")
+@Route(value = "registro", layout = AppLayoutNavbar.class)
 @Uses(Icon.class)
-public class Registro extends HorizontalLayout {
+public class Registro extends VerticalLayout {
 
     public Registro() {
 
@@ -45,38 +45,40 @@ public class Registro extends HorizontalLayout {
         FechaNacimiento.setLabel("Fecha de Nacimiento");
         TextField DNI = new TextField();
         DNI.setLabel("DNI");
-        VerticalLayout vl1 = new VerticalLayout();
-        VerticalLayout vl2 = new VerticalLayout();
         Button boton = new Button();
-
-
-
-        Nombre.setWidthFull();
-        Apellido.setWidthFull();
-        Correo.setWidthFull();
-        Telefono.setWidthFull();
-        Direccion.setWidthFull();
-        Contrasena.setWidthFull();
-        ConfirmarContrasena.setWidthFull();
-        FechaNacimiento.setWidthFull();
-        DNI.setWidthFull();
-        vl1.add(Nombre,Apellido,Correo,Telefono,Direccion);
         boton.setText("Registrarse");
         boton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        vl2.add(Contrasena,ConfirmarContrasena,FechaNacimiento,DNI,boton);
+        VerticalLayout vl2 = new VerticalLayout();
+        vl2.add(boton);
 
-        vl1.setHeightFull();
-        vl2.setHeightFull();
-        vl1.setAlignItems(Alignment.END);
-        vl2.setAlignItems(Alignment.START);
-        boton.getStyle().set("margin-top", "20px");
-        vl1.getStyle().set("margin-top", "20px");
-        vl2.getStyle().set("margin-top", "20px");
+
+        Nombre.getStyle().set("padding", "var(--lumo-space-s)");
+        Apellido.getStyle().set("padding", "var(--lumo-space-s)");
+        Correo.getStyle().set("padding", "var(--lumo-space-s)");
+        Telefono.getStyle().set("padding", "var(--lumo-space-s)");
+        Direccion.getStyle().set("padding", "var(--lumo-space-s)");
+        Contrasena.getStyle().set("padding", "var(--lumo-space-s)");
+        ConfirmarContrasena.getStyle().set("padding", "var(--lumo-space-s)");
+        FechaNacimiento.getStyle().set("padding", "var(--lumo-space-s)");
+        DNI.getStyle().set("padding", "var(--lumo-space-s)");
+
+        Nombre.setWidth("500px");
+        Apellido.setWidth("500px");
+        Correo.setWidth("500px");
+        Telefono.setWidth("500px");
+        Direccion.setWidth("500px");
+        Contrasena.setWidth("500px");
+        ConfirmarContrasena.setWidth("500px");
+        FechaNacimiento.setWidth("500px");
+        DNI.setWidth("500px");
+        boton.setWidth("500px");
+
         setAlignItems(Alignment.CENTER);
 
-        add(vl1,vl2);
-
-
+        vl2.add(Nombre, Apellido, Correo, Telefono, Direccion, Contrasena, ConfirmarContrasena, FechaNacimiento, DNI);
+        vl2.setAlignItems(Alignment.CENTER);
+        vl2.setWidth("69%");
+        add(vl2,boton);
 
     }
 }
