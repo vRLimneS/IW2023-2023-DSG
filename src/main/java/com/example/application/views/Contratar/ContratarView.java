c6c1255988756f60a7e25f9bd7074f33735696ff
LimneS <85122733+vRLimneS@users.noreply.github.com>
2023-11-28 11:43:58 +0100

    Mejoras Registro y contratar
    Mejorada la vista del registro conforme a los datos de contraseña numeros de telefono correo y implementado el inicio de la vista contratar

--- src/main/java/com/example/application/views/Contratar/ContratarView.java
+++ src/main/java/com/example/application/views/Contratar/ContratarView.java
@@ -1,46 +1,50 @@
 package com.example.application.views.Contratar;
 
 import com.example.application.views.AppLayoutNavbar;
-import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.textfield.IntegerField;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.component.tabs.TabsVariant;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.dom.Style;
 import com.vaadin.flow.router.Route;
-
-
-@PageTitle("Contratar")
 @Route(value = "Contratar", layout = AppLayoutNavbar.class)
-public class ContratarView extends HorizontalLayout {
-
-    private final TextField name;
-    private final Button sayHello;
+public class ContratarView extends VerticalLayout {
 
     public ContratarView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
+        Tab analytics = new Tab("Analytics");
+        Tab customers = new Tab("Customers");
+        Tab dashboards = new Tab("Dashboards");
+        Tab documents = new Tab("Documents");
+        Tab orders = new Tab("Orders");
+        Tab products = new Tab("Products");
+        Tab tasks = new Tab("Tasks");
+
+        Tabs tabs = new Tabs(analytics, customers, dashboards, documents,
+                orders, products, tasks);
+        tabs.addThemeVariants(TabsVariant.LUMO_HIDE_SCROLL_BUTTONS);
+        tabs.setMaxWidth("100%");
+        VerticalLayout layout = new VerticalLayout();
+        layout.add(tabs);
+        layout.setAlignItems(Alignment.CENTER);
+        layout.getElement().getStyle().set("padding", "0px");
+
+
+        VerticalLayout vl2 = new VerticalLayout();
+        tabs.addSelectedChangeListener(event -> {
+            mostrarTab(event.getSelectedTab(), vl2);
         });
-        sayHello.addClickShortcut(Key.ENTER);
+        layout.add(vl2);
+        add(layout);
+    }
 
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
-
-        add(name, sayHello);
-
-        IntegerField integerField = new IntegerField();
-        integerField.setLabel("Quantity");
-        integerField.setHelperText("Max 10 items");
-        integerField.setMin(0);
-        integerField.setMax(10);
-        integerField.setValue(2);
-        integerField.setStepButtonsVisible(true);
-        add(integerField);
-
-
+    public void mostrarTab(Tab tab, VerticalLayout vl2){
+        vl2.removeAll();
+        TextField text = new TextField();
+        text.setValue(tab.getLabel());
+        text.setReadOnly(true);
+        vl2.setAlignItems(Alignment.START);
+        vl2.add(text);
     }
 
 }
--- src/main/java/com/example/application/views/Registro/Registro.java
+++ src/main/java/com/example/application/views/Registro/Registro.java
@@ -1,22 +1,17 @@
 package com.example.application.views.Registro;
 
 import com.example.application.views.AppLayoutNavbar;
-import com.vaadin.flow.component.Composite;
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.datepicker.DatePicker;
-import com.vaadin.flow.component.dependency.Uses;
-import com.vaadin.flow.component.html.H3;
-import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
-import com.vaadin.flow.theme.lumo.LumoUtility.Padding;
 
 @PageTitle("registro")
 @Route(value = "registro", layout = AppLayoutNavbar.class)
@@ -29,22 +24,31 @@ public class Registro extends VerticalLayout {
 
         TextField Nombre = new TextField();
         Nombre.setLabel("Nombre");
+
         TextField Apellido = new TextField();
         Apellido.setLabel("Apellido");
-        TextField Correo = new TextField();
+
+        EmailField Correo = new EmailField();
         Correo.setLabel("Correo Electronico");
+
         TextField Telefono = new TextField();
         Telefono.setLabel("Telefono");
+
         TextField Direccion = new TextField();
         Direccion.setLabel("Direccion");
-        TextField Contrasena = new TextField();
+
+        PasswordField Contrasena = new PasswordField();
         Contrasena.setLabel("Contraseña");
-        TextField ConfirmarContrasena = new TextField();
+
+        PasswordField ConfirmarContrasena = new PasswordField();
         ConfirmarContrasena.setLabel("Confirmar Contraseña");
+
         DatePicker FechaNacimiento = new DatePicker();
         FechaNacimiento.setLabel("Fecha de Nacimiento");
+
         TextField DNI = new TextField();
         DNI.setLabel("DNI");
+
         Button boton = new Button();
         boton.setText("Registrarse");
         boton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
@@ -72,10 +76,12 @@ public class Registro extends VerticalLayout {
         FechaNacimiento.setWidth("500px");
         DNI.setWidth("500px");
         boton.setWidth("500px");
+        Telefono.setMinLength(9);
+        Telefono.setMaxLength(9);
 
         setAlignItems(Alignment.CENTER);
 
-        vl2.add(Nombre, Apellido, Correo, Telefono, Direccion, Contrasena, ConfirmarContrasena, FechaNacimiento, DNI);
+        vl2.add(Nombre, Apellido, Correo, Contrasena, ConfirmarContrasena, Telefono, Direccion,FechaNacimiento, DNI);
         vl2.setAlignItems(Alignment.CENTER);
         vl2.setWidth("69%");
         add(vl2,boton);
