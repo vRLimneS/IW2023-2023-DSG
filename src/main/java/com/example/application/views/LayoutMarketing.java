4cbfff9e9b72030bd3206145d8ae430926290c39
gonbroy <gonzalo.barbarodriguez@alum.uca.es>
2023-12-04 12:48:43 +0100

    Vista CrearTarifas terminada

--- src/main/java/com/example/application/views/LayoutMarketing.java
+++ src/main/java/com/example/application/views/LayoutMarketing.java
@@ -1,5 +1,6 @@
 package com.example.application.views;
 
+import com.example.application.views.Marketing.CrearTarifas;
 import com.example.application.views.Marketing.PrivateTarifasView;
 import com.example.application.views.login.LoginBasic;
 import com.vaadin.flow.component.applayout.AppLayout;
@@ -7,6 +8,7 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.RouterLink;
+
 
 public class LayoutMarketing extends AppLayout {
 
@@ -28,19 +30,18 @@ public class LayoutMarketing extends AppLayout {
         Tabs tabs = new Tabs();
         tabs.getStyle().set("margin", "auto");
 
-
         //Creamos los routerlinks de las pestañas posibles conectados a las clases
 
+        RouterLink crearTarifasLink = new RouterLink("Crear", CrearTarifas.class);
         RouterLink TarifasLink = new RouterLink("Tarifas", PrivateTarifasView.class);
         RouterLink loginlink = new RouterLink("Login", LoginBasic.class);
-
         //Creamos la tabla asociada a esa ruta y la unimos a la tabla general del layout
 
         Tab TarifasTab = new Tab(TarifasLink);
         Tab logintab = new Tab(loginlink);
+        Tab crearTarifasTab = new Tab(crearTarifasLink);
 
-
-        tabs.add(TarifasTab, logintab);
+        tabs.add(crearTarifasTab, TarifasTab, logintab);
 
         return tabs;
     }
--- src/main/java/com/example/application/views/Marketing/CrearTarifas.java
+++ src/main/java/com/example/application/views/Marketing/CrearTarifas.java
@@ -1,8 +1,63 @@
 package com.example.application.views.Marketing;
 
+import com.example.application.views.LayoutMarketing;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-@Route(value = "creartarifas")
-public class CrearTarifas {
-    
+import java.awt.*;
+
+@Route(value = "creartarifas", layout = LayoutMarketing.class)
+public class CrearTarifas extends VerticalLayout {
+    public CrearTarifas(){
+        H1 title = new H1("Crear Tarifas");
+        add(title);
+        TextField NombreTarifa = new TextField();
+        NombreTarifa.setLabel("Nombre Tarifa");
+        TextField PrecioTarifa = new TextField();
+        PrecioTarifa.setLabel("Precio Tarifa");
+        TextField MinutosMovil = new TextField();
+        MinutosMovil.setLabel("Minutos Movil");
+        TextField Datos = new TextField();
+        Datos.setLabel("Datos");
+        TextField SMS = new TextField();
+        SMS.setLabel("SMS");
+        TextField MinutosFijo = new TextField();
+        MinutosFijo.setLabel("Minutos Fijo");
+        TextField VelocidadFibra = new TextField();
+        VelocidadFibra.setLabel("Velocidad Fibra");
+
+        com.vaadin.flow.component.button.Button boton = new Button();
+        boton.setText("Crear Tarifa");
+        boton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        VerticalLayout vl2 = new VerticalLayout();
+        vl2.add(boton);
+
+        NombreTarifa.getStyle().set("padding", "var(--lumo-space-s)");
+        PrecioTarifa.getStyle().set("padding", "var(--lumo-space-s)");
+        MinutosMovil.getStyle().set("padding", "var(--lumo-space-s)");
+        Datos.getStyle().set("padding", "var(--lumo-space-s)");
+        SMS.getStyle().set("padding", "var(--lumo-space-s)");
+        MinutosFijo.getStyle().set("padding", "var(--lumo-space-s)");
+        VelocidadFibra.getStyle().set("padding", "var(--lumo-space-s)");
+        boton.getStyle().set("padding", "var(--lumo-space-s)");
+
+        NombreTarifa.setWidth("50%");
+        PrecioTarifa.setWidth("50%");
+        MinutosMovil.setWidth("50%");
+        Datos.setWidth("50%");
+        SMS.setWidth("50%");
+        MinutosFijo.setWidth("50%");
+        VelocidadFibra.setWidth("50%");
+
+        add(NombreTarifa, PrecioTarifa, MinutosMovil, Datos, SMS, MinutosFijo, VelocidadFibra, boton);
+        setHorizontalComponentAlignment(Alignment.CENTER, title, NombreTarifa, PrecioTarifa, MinutosMovil, Datos, SMS, MinutosFijo, VelocidadFibra, boton);
+        setSizeFull();
+
+    }
+
 }
+
