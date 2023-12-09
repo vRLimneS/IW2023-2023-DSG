f25221c3a9ecc45ec7032bc38ad7184b88215275
LimneS <85122733+vRLimneS@users.noreply.github.com>
2023-12-09 17:50:36 +0100

    vista tarifas

--- src/main/java/com/example/application/views/DepATC/TarifasATC.java
+++ src/main/java/com/example/application/views/DepATC/TarifasATC.java
@@ -1,5 +1,6 @@
 package com.example.application.views.DepATC;
 
+import com.example.application.data.TarifaService;
 import com.example.application.views.Comunes.TarifasView;
 import com.example.application.views.Layouts.LayoutPrincipal;
 import com.vaadin.flow.router.Route;
@@ -9,4 +10,10 @@ import jakarta.annotation.security.RolesAllowed;
 @RolesAllowed("ATCCLT")
 @Route(value = "TarifasATC", layout = LayoutPrincipal.class)
 public class TarifasATC extends TarifasView {
+
+    private TarifaService tarifaService;
+
+    public TarifasATC(TarifaService tarifaService) {
+        super(tarifaService);
+    }
 }
--- src/main/java/com/example/application/views/Marketing/PrivateTarifasView.java
+++ src/main/java/com/example/application/views/Marketing/PrivateTarifasView.java
@@ -1,5 +1,7 @@
 package com.example.application.views.Marketing;
 
+import com.example.application.data.TarifaService;
+import com.example.application.views.DepATC.TarifasATC;
 import com.example.application.views.Layouts.LayoutPrincipal;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.RolesAllowed;
@@ -7,8 +9,11 @@ import jakarta.annotation.security.RolesAllowed;
 @RolesAllowed("MARKETING")
 @Route(value = "TarifasPr", layout = LayoutPrincipal.class)
 public class PrivateTarifasView extends com.example.application.views.Comunes.TarifasView {
-    public PrivateTarifasView(){
-        super();
+
+    private TarifaService tarifasService;
+
+    public PrivateTarifasView(TarifaService tarifaService){
+        super(tarifaService);
 
     }
 
